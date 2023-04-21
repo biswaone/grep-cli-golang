d7471ac7dfde4b2d5860d449f38fcfc429dd42f9
Biswa Prakash Mishra <biswaprakash444@gmail.com>
2023-04-21 22:12:44 +0530

    reursively traverse directory to grep

--- cmd/root.go
+++ cmd/root.go
@@ -7,9 +7,12 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 	"regexp"
+	"sync"
 
 	"github.com/spf13/cobra"
 )
@@ -28,6 +31,58 @@ var rootCmd = &cobra.Command{
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
+		// recursively read from directory
+		searchStr := args[0]
+		path := args[1]
+		fileInfo, err := os.Stat(path)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		mode := fileInfo.Mode()
+		// grep from directory
+		if mode.IsDir() {
+
+			result := make(map[string][]string)
+
+			var mu sync.Mutex
+			var wg sync.WaitGroup
+			err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+				if err != nil {
+					fmt.Printf("Error: %v\n", err)
+					return err
+				}
+				if !d.IsDir() {
+					wg.Add(1)
+
+					go func() {
+						defer wg.Done()
+						file, err := os.Open(path)
+						if err != nil {
+							fmt.Fprintf(os.Stderr, "failed to open file %q: %v\n", path, err)
+							return
+						}
+						defer file.Close()
+						Strarr, _ := grep(file, searchStr, flag)
+						mu.Lock()
+						result[path] = Strarr
+						mu.Unlock()
+					}()
+				}
+				return nil
+
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+			wg.Wait()
+			for key, value := range result {
+				for _, v := range value {
+					fmt.Println(key, v)
+				}
+			}
+		}
+
 		// grep from stdin
 		if len(args) == 1 {
 			Strarr, err := grep(os.Stdin, args[0], flag)
@@ -37,7 +92,7 @@ var rootCmd = &cobra.Command{
 			fmt.Println(Strarr)
 		}
 		// grep from files
-		if len(args) > 0 {
+		if !mode.IsDir() {
 			for _, arg := range args[1:] {
 				file, err := os.Open(arg)
 				if err != nil {
@@ -91,4 +146,5 @@ func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	rootCmd.Flags().BoolVarP(&flag.outputToFile, "output", "o", false, "output to file")
 	rootCmd.Flags().BoolVarP(&flag.caseSensitive, "case-sensitive", "i", false, "case sensitive")
+
 }
